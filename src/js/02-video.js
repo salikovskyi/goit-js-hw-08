dafe504279624a6b1cef9e30cdd900b7f1683648
salikovskyi <osalikovsiy@gmail.com>
2021-11-16 14:10:38 +0300

    zaza

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,29 @@
-const throttle = require('lodash.throttle');
-import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
+const VIDEO_KEY = 'videoplayer-current-time';
 
-    const onPlay = function(data) {  
-  localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+const onPlay = function (data) {
+  const time = JSON.stringify(data.seconds);
+  localStorage.setItem(VIDEO_KEY, time);
 };
-
-
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const saveSet = localStorage.getItem("videoplayer-current-time");
-const parsedSettings = JSON.parse(saveSet);
-console.log(parsedSettings);
+const saveTime = localStorage.getItem(VIDEO_KEY);
 
-player.setCurrentTime(parsedSettings).then(function (seconds) {
-  
-    if (saveTime !== null) {
-        player.setCurrentTime(saveTime);
-          console.log(seconds);
-}
-});
-
-
+player
+  .setCurrentTime(saveTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video's duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,29 @@
 import throttle from 'lodash.throttle';
 
-
-
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector(".feedback-form input")
-
 };
-const STORAGE_KEY = "feedba0ck-form-state";
 
+const STORAGE_KEY = "feedba0ck-form-state";
+populateTextarea();
 let formData = {};
 
-
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-
-refs.form.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value;
-})
-
-populateTextarea();
-
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    formData.email = refs.input.value;
-    formData.message = refs.textarea.value;
-      console.log(formData)
+    console.log(formData)
+    formData = {};
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-
-
 };
 
 
 function onTextareaInput(evt) {
-      formData[evt.target.name] = evt.target.value
+    formData[evt.target.name] = evt.target.value;
   const saveDataEl = JSON.stringify(formData);
   localStorage.setItem(STORAGE_KEY, saveDataEl);
 };
@@ -47,10 +33,11 @@ function populateTextarea() {
    
     if (savedMessage) {
         const pasrsedSav = JSON.parse(savedMessage);
-        let formData ={};
-        formData = pasrsedSav;
-        refs.textarea.value = pasrsedSav.message;
-        refs.input.value = pasrsedSav.email;
+        const keys = Object.keys(pasrsedSav);
+
+        for (const key of keys) {
+            refs.form.elements[key].value = pasrsedSav[key];
+        }
     }
 }
 
